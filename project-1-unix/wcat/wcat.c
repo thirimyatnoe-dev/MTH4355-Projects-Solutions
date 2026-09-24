1a22fb484e4a92a40f9a2e5ab3300c9a4c9d7586
Thiri Myat Noe <thirimyatnoe@Mac.lan>
2026-09-23 20:36:43 -0400

    Update wcat

--- project-1-unix/wcat/wcat.c
+++ project-1-unix/wcat/wcat.c
@@ -7,22 +7,16 @@ int main(int argc, char *argv[]) {
     }
 
     for (int i = 1; i < argc; i++) {
-
         FILE *fp = fopen(argv[i], "r");
-
         if (fp == NULL) {
             printf("wcat: cannot open file\n");
             return 1;
         }
-
-        char buffer[1024];
-
+        char buffer[2000];
         while (fgets(buffer, sizeof(buffer), fp) != NULL) { //read line from the file, fp and then put it into buffer?
             printf("%s", buffer);
         }
-
         fclose(fp);
     }
-
     return 0;
 }
